090508796790b1610b6baf5e86f78805d5fba41b
Janne Leppänen <janne@janneleppanen.com>
2020-01-19 14:28:06 +0200

    Add standardization for knn

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -2,10 +2,17 @@ require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 const loadCSV = require("./load-csv");
 
+const sampleSize = 200;
+
 function knn(features, labels, predictionPoint, k) {
+  const { mean, variance } = tf.moments(features, 0);
+  const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
+
   return (
     features
-      .sub(predictionPoint)
+      .sub(mean)
+      .div(variance.pow(0.5))
+      .sub(scaledPrediction)
       .pow(2)
       .sum(1)
       .pow(0.5)
@@ -23,17 +30,17 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   {
     shuffle: true,
     splitTest: 10,
-    dataColumns: ["lat", "long"],
+    dataColumns: ["lat", "long", "sqft_lot", "sqft_living"],
     labelColumns: ["price"]
   }
 );
 
-features = tf.tensor(features);
-labels = tf.tensor(labels);
+features = tf.tensor(features.slice(0, sampleSize));
+labels = tf.tensor(labels.slice(0, sampleSize));
 
 testFeatures.forEach((testPoint, i) => {
   const price = knn(features, labels, tf.tensor(testPoint), 2);
   const err = (testLabels[i][0] - price) / testLabels[i][0];
-  console.log("Guess", price, testLabels[0][0]);
+  // console.log("Guess", price, testLabels[0][0]);
   console.log("Error", (err * 100).toFixed(2) + "%");
 });
