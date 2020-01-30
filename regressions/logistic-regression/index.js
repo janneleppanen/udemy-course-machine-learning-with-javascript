ae24361997d75b2ee9914d49c2e3d9087a6b663c
Janne Leppänen <janne@janneleppanen.com>
2020-01-30 18:46:48 +0200

    Update logistic regression test function

--- regressions/logistic-regression/index.js
+++ regressions/logistic-regression/index.js
@@ -23,9 +23,4 @@ const regression = new LogisticRegression(features, labels, {
 });
 
 regression.train();
-regression
-  .predict([
-    [130, 306, 1.75],
-    [88, 97, 1.065]
-  ])
-  .print();
+console.log(regression.test(testFeatures, testLabels));
